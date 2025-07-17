594e95ab61bd228125a706e192e903302da00f29
ahaoboy <504595380@qq.com>
2025-07-17 15:04:37 +0800

    add get_exe_ico

--- src/exe.rs
+++ src/exe.rs
@@ -6,18 +6,10 @@ pub fn get_exe_ico(exe_path: &str) -> anyhow::Result<Vec<u8>> {
     let pwsh = format!(
         r#"Add-Type -AssemblyName System.Drawing; $icon=[System.Drawing.Icon]::ExtractAssociatedIcon('{exe_path}'); $ms = New-Object System.IO.MemoryStream; $icon.Save($ms); [Convert]::ToBase64String($ms.ToArray())"#
     );
-
     let output = Command::new("powershell")
         .args(["-NoProfile", "-Command", &pwsh])
         .output()?;
-
     let stdout = String::from_utf8_lossy(&output.stdout);
-
-    let icon_bytes = general_purpose::STANDARD
-        .decode(stdout.trim())
-        .expect("base64 decode failed");
-
-    println!("Extracted icon bytes: {} bytes", icon_bytes.len());
-
+    let icon_bytes = general_purpose::STANDARD.decode(stdout.trim())?;
     Ok(icon_bytes)
 }
